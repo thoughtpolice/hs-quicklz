915ee56ead4516b263ddca6c9144e40dad4c853b
austin seipp <as@hacks.yi.org>
2011-03-14 02:17:54 -0500

    rewrite benchmarks and split up compression/decompression benchmarks

--- bench/bench1.hs
+++ bench/bench1.hs
@@ -9,14 +9,29 @@ import qualified Data.ByteString.Lazy.Char8 as L
 
 import qualified Codec.Compression.QuickLZ as QLZ
 import qualified Codec.Compression.Zlib as Zlib
+--import qualified Codec.Compression.BZip as BZip
 
-main = defaultMain [
-    bench "quicklz /usr/share/dict/words" quicklz
-  , bench "zlib /usr/share/dict/words" zlib
-  ]
-  where quicklz = do
-          !x <- (S.length . QLZ.compress) `liftM` S.readFile "words"
-          return ()
-        zlib = do
-          !x <- (L.length . Zlib.compress) `liftM` L.readFile "words"
-          return ()
+main :: IO ()
+main = 
+  defaultMain [ bgroup "compression speed" [
+                  bench "quicklz compress words" quicklz_comp
+                , bench "zlib compress words" zlib_comp
+--              , bench "bzip compress words" bzip_comp
+                ]
+              , bgroup "decompress speed" [
+                  bench "quicklz decompress words" quicklz_decomp
+                , bench "quicklz overlap decompress words" quicklz_overlap_decomp
+                , bench "zlib decompress words" zlib_decomp
+--              , bench "bzip decompress words" bzip_decomp
+                ]
+              ]
+  where quicklz_comp   = nfIO $ liftS QLZ.compress "words"
+        quicklz_decomp = nfIO $ liftS QLZ.decompress "words_quicklz"
+        quicklz_overlap_decomp = nfIO $ liftS QLZ.decompress' "words_quicklz"
+        
+        zlib_comp   = nfIO $ liftL Zlib.compress "words"
+        zlib_decomp = nfIO $ liftL Zlib.decompress "words_zlib"
+
+-- utilities
+liftS c f = (S.length . c) `liftM` S.readFile f
+liftL c f = (L.length . c) `liftM` L.readFile f
